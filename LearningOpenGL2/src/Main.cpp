7a6d39006d3b775486f3e14dc183281cf969b2a1
Satwik <satwik292007@gmail.com>
2025-06-12 17:16:32 +0530

    Launch a window with a basic rectangle.

--- LearningOpenGL2/src/Main.cpp
+++ LearningOpenGL2/src/Main.cpp
@@ -1,6 +1,118 @@
 #include <iostream>
 
-int main()
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+int main(void)
 {
-	std::cout << "Hello, World!" << std::endl;
+    const char* vertexShaderSource = 
+        "#version 460 core\n"
+        "layout (location = 0) in vec3 pos;\n"
+        "void main() {\n"
+        "gl_Position = vec4(pos.x, pos.y, pos.z, 1.0f); }\0";
+    const char* fragShaderSource = 
+        "#version 460 core\n"
+        "out vec4 FragColor;\n"
+        "void main() {\n"
+        "FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f); }\0";
+
+    float vertices[] = { 
+        0.5f,  0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f 
+    };
+
+    unsigned int indices[] = { 
+        0, 1, 2,
+        0, 3, 2 
+    };
+
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW.\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "LearningOpenGL2", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Failed to create window.\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    int version = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    if (version == 0) 
+    {
+        std::cout << "Failed to create window.\n";
+        glfwTerminate();
+        return -1; 
+    }
+
+    std::cout << glGetString(GL_VERSION) << '\n';
+
+    unsigned int vertexShader, fragShader;
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragShader, 1, &fragShaderSource, NULL);
+    glCompileShader(fragShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragShader);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+    glUseProgram(shaderProgram);
+    
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
